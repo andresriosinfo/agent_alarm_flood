97546df84c156a1acffdc913058f46fdd5715ed7
andresriosinfo <andres_rios@info-design.co>
2026-03-07 13:53:57 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -18,13 +18,7 @@ from src.data_loader import load_alarms
 from src.baseline_cache import get_or_create_baseline
 from src.operational_agent import assess_current_state
 
-import inspect
-st.write(inspect.signature(get_or_create_baseline))
-st.set_page_config(
-    page_title="Inteligencia de Alarmas",
-    layout="wide",
-    initial_sidebar_state="collapsed",
-)
+
 
 st.markdown("""
 <style>
